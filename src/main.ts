3e95fd0aaf5a318faf8f411b527eec95de204c96
Robert Matos <robertvmatos@gmail.com>
2025-06-05 07:56:34 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
+import express from 'express'; // ou import express = require('express');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import express from 'express';
 import { createServer, proxy } from 'aws-serverless-express';
 import { Handler } from 'aws-lambda';
 
 const expressApp = express();
 let cachedServer: Handler;
 
+expressApp.get('/', (_req: express.Request, res: express.Response) => res.send('✅ NestJS Serverless ativo'));
+expressApp.get('/healthz', (_req: express.Request, res: express.Response) => res.sendStatus(200));
+
 async function bootstrapServer(): Promise<Handler> {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
 
   app.enableCors();
-
-  // Rota básica para health check e validação
-  expressApp.get('/', (_req, res) => res.send('✅ NestJS Serverless ativo'));
-  expressApp.get('/healthz', (_req, res) => res.sendStatus(200));
 
   const config = new DocumentBuilder()
     .setTitle('Minha API')
@@ -46,6 +45,5 @@ export const handler = async (event: any, context: any) => {
   if (!cachedServer) {
     cachedServer = await bootstrapServer();
   }
-
   return proxy(cachedServer, event, context, 'PROMISE').promise;
 };
